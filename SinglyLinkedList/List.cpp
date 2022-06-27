a41d16383eef56295acbbea4f1768c4331a429d6
Sergey Evseev <sevseev@gmail.com>
2022-06-27 21:21:47 +0300

    Class Commit

--- SinglyLinkedList/List.cpp
+++ SinglyLinkedList/List.cpp
@@ -53,3 +53,52 @@ int List::GetCount()
 {
 	return Count; //Возвращаем количество элементов
 }
+
+void List::Insert(int pos)
+{
+	if (pos == 0) //если параметр отсутствует или равен 0, то запрашиваем его
+	{
+		cout << "Input position: ";
+		cin >> pos;
+	}
+	if (pos < 1 || pos > Count + 1) //Позиция от 1 до Count?
+	{
+		cout << "Incorrect position !!!\n"; //Неверная позиция
+		return;
+	}
+	if (pos == Count + 1) //Если вставка в конец списка
+	{
+		int data; //Вставляемые данные
+		cout << "Input new number: ";
+		cin >> data;
+		Add(data); //Добавление в конец списка
+		return;
+	}
+	else if (pos == 1)
+	{
+		int data; //Вставляемые данные
+		cout << "Input new number: ";
+		cin >> data; //Добавление в начало списка		
+		Add(data);
+		return;
+
+	}
+	int i = 1; //Счетчик
+	Element* Ins = Head; //Отсчитываем от головы n - 1 элементов
+	while (i < pos)
+	{
+		Ins = Ins->next; //Доходим до элемента, перед которым вставляемся
+		i++;
+	}
+	Element* PrevIns = Ins->prev; //Доходим до элемента, который предшествует
+	Element* temp = new Element; //Создаем новый элемент
+	//Вводим данные
+	cout << "Input new number: ";
+	cin >> temp->data;
+	if (PrevIns != 0 && Count != 1) //настройка связей
+		PrevIns->next = temp;
+	temp->next = Ins;
+	temp->prev = PrevIns;
+	Ins->prev = temp;
+	Count++;
+}
